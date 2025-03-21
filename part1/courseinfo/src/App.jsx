0a92c2df45ac86ab7d938f4b47d53b6af93fdb80
Akash Raj <ebookexer@gmail.com>
2025-03-21 20:38:29 +0530

    Complete courseinfo exercises

---
+++ part1/courseinfo/src/App.jsx
@@ -0,0 +1,44 @@
+const Header = ({course}) => <h1>{course}</h1>;
+
+const Part = ({part, exercises}) => <p>{part} {exercises}</p>
+const Content = ({parts}) => {
+  const [part1, part2, part3] = parts;
+  return (
+    <>
+      <Part part={part1.name} exercises={part1.exercises} />
+      <Part part={part2.name} exercises={part2.exercises} />
+      <Part part={part3.name} exercises={part3.exercises} />
+    </>
+  )
+}
+
+const Total = ({parts}) => <p>Number of exercises {parts.reduce((acc, curr, index) => acc + parts[index].exercises, 0)}</p>
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+  return (
+    <div>
+      <Header course={course.name}/>
+      <Content parts={course.parts}/>
+      <Total parts={course.parts} />
+    </div>
+  )
+}
+
+export default App
